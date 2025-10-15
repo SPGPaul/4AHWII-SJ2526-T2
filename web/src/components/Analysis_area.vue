0f7f008739b472dbb4b65a3133985b6a3843a07e
SPGPaul <FIA220266@spengergasse.at>
2025-10-15 11:25:48 +0200

    working

--- web/src/components/Analysis_area.vue
+++ web/src/components/Analysis_area.vue
@@ -7,6 +7,11 @@ import { CanvasRenderer } from 'echarts/renderers'
 import { BarChart } from 'echarts/charts'
 import { TitleComponent, TooltipComponent, GridComponent } from 'echarts/components'
 import { UniversalTransition } from 'echarts/features'
+import * as echarts from 'echarts/core'
+import { onMounted, ref } from 'vue'
+import { watch } from 'vue'
+import { useBackgroundColor } from 'vuetify/lib/composables/color.mjs'
+import { collect } from 'echarts/types/src/component/axisPointer/modelHelper.js'
 
 use([
     CanvasRenderer,
@@ -17,23 +22,24 @@ use([
     UniversalTransition
 ])
 
-import * as echarts from 'echarts/core'
-import { onMounted, ref } from 'vue'
-import { watch } from 'vue'
+
 const chartRef = ref(null)
 const series = [
     {
         data: [120, 200, 150, 80, 70, 110, 130],
         type: 'bar',
         itemStyle: {
-            color: 'white'
-        }
+            color: 'red'
+        },
     }
 ]
 
 const options = {
+    text:{
+        color: "white"
+    },
     title: {
-        text: 'ECharts entry example'
+        text: 'Ausgaben'
     },
     tooltip: {
         trigger: 'axis',
@@ -49,7 +55,7 @@ const options = {
     },
     xAxis: {
         type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        data: ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag']
     },
     yAxis: {
         type: 'value'
@@ -71,21 +77,9 @@ onMounted(() => {
 watch(() => options, (val) => {
     console.log('Chart options changed:', val)
 })
-onMounted(() => {
-    console.log('echarts import:', echarts)
-    console.log('VChart component should be mounted now')
-    console.log('chartRef.value:', chartRef.value)
-    if (chartRef.value && chartRef.value.echarts) {
-        console.log('ECharts instance:', chartRef.value.echarts)
-    } else {
-        console.warn('VChart ref or echarts instance not found!')
-    }
-})
 </script>
 
 <template>
     <h2>Analysis Area Component</h2>
-        <VChart ref="chartRef" :options="options" style="height: 400px; width: 100%; background: #222;" />
-        <div ref="nativeChartDiv" style="height: 400px; width: 100%; background: #444; margin-top: 16px;">Native ECharts Test</div>
-    <p>This is a paragraph below the chart.</p>
+    <div ref="nativeChartDiv" style="height: 400px; width: 40%; background: #444; margin-top: 16px;">Ausgaben </div>
 </template>
