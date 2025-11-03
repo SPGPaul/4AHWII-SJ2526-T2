61837df65e8f3f45bd84984841965c7e771bbbc5
XGAMARez <beg220262@spengergasse.at>
2025-11-03 11:37:28 +0100

    Bilder und Tabelle

--- web/src/components/HelloWorld.vue
+++ web/src/components/HelloWorld.vue
@@ -1,24 +1,82 @@
 <template>
-  <v-card height="200">
-    <v-toolbar>
-      <v-toolbar-title text="Toolbar"></v-toolbar-title>
+  <div>
+    <h1>Übersicht</h1>
+    <v-text-field label="Label" variant="outlined" ></v-text-field>
 
-      <template v-slot:append>
-        <v-btn icon="mdi-magnify"></v-btn>
 
-        <v-btn icon="mdi-heart"></v-btn>
 
-        <v-btn icon="mdi-dots-vertical"></v-btn>
-      </template>
-    </v-toolbar>
-  </v-card>
-  <v-container fluid="true">
-    <v-row no-gutters>
-      <v-col v-for="n in 2" key="n">
-        <v-sheet class="pa-2 ma-2">
-          .v-col-auto
-        </v-sheet>
-      </v-col>
-    </v-row>
-  </v-container>
+    <v-table>
+      <thead>
+        <tr>
+          
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in desserts"
+          :key="item.img"
+        >
+          <td>
+            <v-img
+              :src="item.img"
+              max-width="300"
+              max-height="300"
+            />
+          </td>
+          <td>{{ item.calories }}</td>
+          
+        </tr>
+      </tbody>
+  </v-table>
+  </div>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        desserts: [
+          {
+            img: 'https://cdn.prod.website-files.com/65c9eb8b18a74904c71e4d8e/66a8889946d967cf63c780fa_6634971f1028e9a64325b236_rechnungsvorlage-kostenlos-word-pdf-excel.webp',
+            calories: 159,
+          },
+          {
+            img: 'https://cdn.prod.website-files.com/65c9eb8b18a74904c71e4d8e/66a8889946d967cf63c780fa_6634971f1028e9a64325b236_rechnungsvorlage-kostenlos-word-pdf-excel.webp',
+            calories: 237,
+          },
+          {
+            img: 'https://cdn.prod.website-files.com/65c9eb8b18a74904c71e4d8e/66a8889946d967cf63c780fa_6634971f1028e9a64325b236_rechnungsvorlage-kostenlos-word-pdf-excel.webp',
+            calories: 262,
+          },
+          {
+            img: 'https://cdn.prod.website-files.com/65c9eb8b18a74904c71e4d8e/66a8889946d967cf63c780fa_6634971f1028e9a64325b236_rechnungsvorlage-kostenlos-word-pdf-excel.webp',
+            calories: 305,
+          },
+          {
+            img: 'https://cdn.prod.website-files.com/65c9eb8b18a74904c71e4d8e/66a8889946d967cf63c780fa_6634971f1028e9a64325b236_rechnungsvorlage-kostenlos-word-pdf-excel.webp',
+            calories: 356,
+          },
+          {
+            img: 'https://cdn.prod.website-files.com/65c9eb8b18a74904c71e4d8e/66a8889946d967cf63c780fa_6634971f1028e9a64325b236_rechnungsvorlage-kostenlos-word-pdf-excel.webp',
+            calories: 375,
+          },
+          {
+            img: 'https://cdn.prod.website-files.com/65c9eb8b18a74904c71e4d8e/66a8889946d967cf63c780fa_6634971f1028e9a64325b236_rechnungsvorlage-kostenlos-word-pdf-excel.webp',
+            calories: 392,
+          },
+          {
+            img: 'https://cdn.prod.website-files.com/65c9eb8b18a74904c71e4d8e/66a8889946d967cf63c780fa_6634971f1028e9a64325b236_rechnungsvorlage-kostenlos-word-pdf-excel.webp',
+            calories: 408,
+          },
+          {
+            img: 'https://cdn.prod.website-files.com/65c9eb8b18a74904c71e4d8e/66a8889946d967cf63c780fa_6634971f1028e9a64325b236_rechnungsvorlage-kostenlos-word-pdf-excel.webp',
+            calories: 452,
+          },
+          {
+            img: 'https://cdn.prod.website-files.com/65c9eb8b18a74904c71e4d8e/66a8889946d967cf63c780fa_6634971f1028e9a64325b236_rechnungsvorlage-kostenlos-word-pdf-excel.webp',
+            calories: 518,
+          },
+        ],
+      }
+    },
+  }
+</script>
